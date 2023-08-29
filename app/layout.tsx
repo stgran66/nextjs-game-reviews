96356097505da3e88a4528aba759cf4805f9ad3c
stgran66 <stanislaw.grygoriew@gmail.com>
2023-08-29 21:23:22 +0200

    added typescript to project, and created some more template pages

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: LayoutProps) {
+  return (
+    <html lang='en'>
+      <body>
+        <header>[header]</header>
+        <main> {children}</main>
+        <footer>[footer]</footer>
+      </body>
+    </html>
+  );
+}
